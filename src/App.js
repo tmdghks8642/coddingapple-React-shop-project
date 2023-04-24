f5af1a929442cc9402037f25d6c68e8f11315f29
이승환 <tmdghks8642@gmail.com>
2023-04-24 23:05:32 +0900

    map 고차함수를 통한 가독성 높이기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container,Nav,Navbar } from 'react-bootstrap';
-import Image1 from './image/IMG_8770.jpeg'
-import Image2 from './image/IMG_9598.jpeg'
-import Image3 from './image/IMG_1111.jpeg'
+
+import dataList from './dataList'
+import { useState } from 'react';
 
 function App() {
+  let [items, setitems] = useState(dataList)
   return (
     <div className="App">
-
     <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">CoZy Shop</Navbar.Brand>
@@ -27,28 +27,34 @@ function App() {
 
      <div className='container' >
       <div className='row'>
-        <div className='col-md-4' >
-          {/* 이미지 경로 설정하는 법 */}
-          <img src={`${Image1}`} width={`80%`} style={{borderRadius:'20px'}}/>
-          <h4>zl존 선그라시</h4>
-          <p>멋짐이 폭발합니다.</p>
-        </div>
-        <div className='col-md-4'>
-        <img src={`${Image2}`} width={`80%`} style={{borderRadius:'20px'}}/>
-          <h4>간zl 선그라시</h4>
-          <p>사람들의 시선을 느껴보세요</p>
-        </div>
-        <div className='col-md-4'>
-        <img src={`${Image3}`} width={`80%`} style={{borderRadius:'20px'}}/>
-          <h4>ToKoy 선그라시</h4>
-          <p>쓰는 순간 차도녀</p>
-        </div>
+      {
+        items.map((el,idx)=>{
+          return <Items key={items[idx]} items={items[idx]} i={idx+1}/>
+        })
+      }
       </div>
      </div>
-
-
     </div>
   );
 }
+
+
+function Items ({items,i}) {
+return (
+  <div className='col-md-4' >
+          {/* 이미지 경로 설정하는 법 */}
+          <img src={'/image/IMG_'+i+'.jpeg'} width={`80%`} style={{borderRadius:'20px'}}/>
+          <h4>{items.title}</h4>
+          <p>{items.content}</p>
+          <p>가격 : {items.price}</p>
+        </div>
+)
+
+}
+
+
+
+
+
 
 export default App;
---
+++ src/dataList.js
@@ -0,0 +1,35 @@
+let data =[
+    {
+      id : 0,
+      title : "zl존 선그라시",
+      content : "멋짐이 폭발합니다.",
+      price : 120000,
+    
+
+    },
+  
+    {
+      id : 1,
+      title : "간zl 선그라시",
+      content : "사람들의 시선을 느껴보세요",
+      price : 110000,
+    
+    },
+  
+    {
+      id : 2,
+      title : "ToKoy 선그라시",
+      content : "쓰는 순간 차도녀",
+      price : 130000,
+    
+    },
+    {
+      id : 3,
+      title : "ToKoy 룩",
+      content : "도쿄 씹어먹는 룩",
+      price : 130000,
+    
+    }
+  ] 
+
+  export default data ;
