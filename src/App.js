e0cc684dd12fd9da9317e52b053a190cd9894cd5
이승환 <tmdghks8642@gmail.com>
2023-04-27 23:28:54 +0900

    styled component 적용과 url파라미터 사

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container,Nav,Navbar } from 'react-bootstrap';
 import dataList from './dataList'
-import { useState } from 'react';
+import { useState} from 'react';
 import { Route,Routes,Link, useNavigate, Outlet } from 'react-router-dom';
 import CART from './cart'
 
 function App() {
   let [items, setitems] = useState(dataList)
   let navigate = useNavigate()
+
+
+
 
 
   return (
@@ -28,50 +31,34 @@ function App() {
       {/* <Link className='link' to='/cart'>CART</Link> */}
       {/* <Link className='link' to='/'>HOME</Link> */}
 
+
       <Routes>
 
       <Route path='/' element={<Main items={items}/>}/>
-      <Route path='/cart' element={<CART/>}/>
+      <Route path='/cart/:id' element={<CART items={items}/>}/>
       {/* 404 페이지 만드는법!  */}
       <Route path='*' element={<div>404 ! </div>}/>
 
-
-
-
+      
             {/* Nested Routes 로 작성 시   */}
-          <Route path='/event' element={<Event/>}>
+          {/* <Route path='/event' element={<Event/>}>
               <Route path='one' element={<div> 첫 주문시 양배추즙 서비스</div>}/>
               <Route path='two' element={<div>생일기념 쿠폰받기</div>}/>
-          </Route>
+          </Route> */}
 
             {/* 그냥 일반 Route로 작성 시  */}
-          <Route path='/event/one' element={<div></div>}/>
-          <Route path='/event/two' element={<div></div>}/>
-
-
-      
+          {/* <Route path='/event/one' element={<div></div>}/>
+          <Route path='/event/two' element={<div></div>}/> */}
     </Routes>
 
-
-  
     </div>
   );
 }
 
 
-function Event (){
-  return (
-    <>
-    <h1>오늘의 이벤트</h1>
-    <Outlet></Outlet>
-    </>
-  )
-}
-
 function Main ({items}){
-
-        return (
-          <> 
+  return (
+    <> 
               <div className='main-bg'>
               <h2 className='title'>Hello!</h2>
               <h2 className='title'>CoZy_Shop!</h2>
@@ -87,28 +74,28 @@ function Main ({items}){
             </div>
             </div>
         </>
-
-        )
-
-}
-
-function Items ({el,i}) {
- 
-return (
-  <div className='col-md-4' >
+        )}
+        
+ function Items ({el,i}) { 
+          return (
+            <div className='col-md-4' >
           {/* 이미지 경로 설정하는 법 */}
           <img src={'/image/IMG_'+i+'.jpeg'} width={`80%`} height={'70%'} style={{borderRadius:'20px'}}/>
           <h4>{el.title}</h4>
           <p>{el.content}</p>
           <p>가격 : {el.price}</p>
         </div>
-)
+)}
 
+// 예시
+function Event (){
+  return (
+    <>
+    <h1>오늘의 이벤트</h1>
+    <Outlet></Outlet>
+    </>
+  )
 }
-
-
-
-
 
 
 export default App;
--- src/cart.js
+++ src/cart.js
@@ -1,17 +1,38 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
-function Cart() {
+
+
+let YellowBtn = styled.button`
+background-color: ${props => props.color};
+color: ${props => props.color === 'black' ? 'white': 'black'};
+padding: 10px;
+
+`
+
+
+
+function Cart({items}) {
+let {id} = useParams();
+// id가 동일한 것 탐색 이러면 정렬 시에도 id 가 동일한 것만 찾음
+let num = items.filter(el=> el.id === Number(id))
+
+
+
 
 return (
 <>
+<YellowBtn color={'black'}>버튼</YellowBtn>
+<YellowBtn color={'yellow'}>버튼</YellowBtn>
 <div className="container">
   <div className="row">
     <div className="col-md-6">
-      <img src='/image/IMG_1.jpeg' width="70%" style={{borderRadius:'20px', marginTop:'10px'}}/>
+      <img src={`/image/IMG_${id}.jpeg`} width="70%" style={{borderRadius:'20px', marginTop:'10px'}}/>
     </div>
     <div className="col-md-6">
-      <h4 className="pt-5">상품명</h4>
-      <p>상품설명</p>
-      <p>120000원</p>
+      <h4 className="pt-5">{num[0].title}</h4>
+      <p>{num[0].content}</p>
+      <p>{num[0].price}원</p>
       <button className="btn btn-danger">주문하기</button> 
     </div>
   </div>
